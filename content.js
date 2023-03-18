145a8aaa57e7b2320a428214a05989c19eb4a956
sk-rw <123981682+sk-rw@users.noreply.github.com>
2023-03-18 16:59:20 -0500

    Refactor formatting to round up for words with 6 or more characters

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ let isFormattingApplied = false;
 
 function toggleFormatting() {
   const paragraphs = document.querySelectorAll('p');
-  
+
   for (let i = 0; i < paragraphs.length; i++) {
     const words = paragraphs[i].innerText.split(' ');
     const formattedWords = words.map(word => {
@@ -10,7 +10,7 @@ function toggleFormatting() {
         const formatted = `<span style="font-weight: bold; font-size: 120%;">${word.substr(0, 3)}</span>${word.substring(3)}`;
         return formatted;
       } else {
-        const halfLength = Math.floor(word.length / 2);
+        const halfLength = Math.ceil(word.length / 2);
         const formatted = `<span style="font-weight: bold; font-size: 120%;">${word.substr(0, halfLength)}</span>${word.substring(halfLength)}`;
         return formatted;
       }
